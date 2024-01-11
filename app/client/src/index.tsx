400a5530ddcc73fd94338677968f7b8f19e35bba
Vinicius Rocha <the.vinicius.rocha@gmail.com>
2024-01-11 16:30:54 -0300

    fix typescript build for app.

--- app/client/src/index.tsx
+++ app/client/src/index.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as Element | DocumentFragment
