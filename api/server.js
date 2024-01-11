fa2dd0db95937c051885671292610a2322fa13d2
Eric Friedrich <44412556+FoxFireShot@users.noreply.github.com>
2024-01-11 14:34:55 -0300

    estrutura inicial

---
+++ api/server.js
@@ -0,0 +1,12 @@
+// api/server.js
+
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send({ name: "Jane Doe" }); // Should be json format
+});
+
+app.listen(3000, () => {
+  console.log("app listening on port 3000");
+});
