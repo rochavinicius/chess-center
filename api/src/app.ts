024544f9e3ecfba2159c9d93cf49b1dd1d8dcac5
Vinicius Rocha <the.vinicius.rocha@gmail.com>
2024-01-12 17:39:04 -0300

    add cors to api

--- api/src/app.ts
+++ api/src/app.ts
@@ -4,6 +4,7 @@ import prisma from "../prisma/prisma"
 import { exit } from "process";
 
 const app = express();
+const cors = require('cors');
 const room_routes = require('./routes/roomRoutes.js')
 const match_routes = require('./routes/matchRoutes.js')
 const board_routes = require('./routes/boardRoutes.js')
@@ -11,6 +12,7 @@ const move_routes = require('./routes/moveRoutes.js')
 
 async function main() {
   app.use(express.json())
+  app.use(cors());
   app.use('/api/room', room_routes)
   app.use('/api/match', match_routes)
   app.use('/api/board', board_routes)
