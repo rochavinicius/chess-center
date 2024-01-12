575ea1761b7514a1df7e984b3e2d41454c92ba98
Vinicius Rocha <the.vinicius.rocha@gmail.com>
2024-01-12 17:27:46 -0300

    add async main to connect explicitly to db.

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,5 +1,7 @@
 // api/app.ts
 import express, {Request, Response, NextFunction} from "express";
+import prisma from "../prisma/prisma"
+import { exit } from "process";
 
 const app = express();
 const room_routes = require('./routes/roomRoutes.js')
@@ -7,18 +9,29 @@ const match_routes = require('./routes/matchRoutes.js')
 const board_routes = require('./routes/boardRoutes.js')
 const move_routes = require('./routes/moveRoutes.js')
 
-app.use(express.json())
-app.use('/api/room', room_routes)
-app.use('/api/match', match_routes)
-app.use('/api/board', board_routes)
-app.use('/api/move', move_routes)
+async function main() {
+  app.use(express.json())
+  app.use('/api/room', room_routes)
+  app.use('/api/match', match_routes)
+  app.use('/api/board', board_routes)
+  app.use('/api/move', move_routes)
 
-// Catch unregistered routes
-app.all('*', (req: Request, res: Response, next: NextFunction) => {
-  //var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-  res.status(404).json({ error: `Route ${req.originalUrl} not found` });
-});
+  // Catch unregistered routes
+  app.all('*', (req: Request, res: Response, next: NextFunction) => {
+    //var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    res.status(404).json({ error: `Route ${req.originalUrl} not found` });
+  });
 
-const PORT = process.env.PORT || 3000
+  const PORT = process.env.PORT || 3000
+  app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
+}
 
-app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
+main()
+  .then(async () => {
+    await prisma.$connect();
+  })
+  .catch(async (e) => {
+    console.error('unpextected error: ', e);
+    await prisma.$disconnect();
+    exit(1)
+  })
