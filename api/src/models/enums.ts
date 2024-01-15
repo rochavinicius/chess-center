a21e98ccb297d6991f28efa39783d54a5cbb8957
Eric Friedrich <44412556+FoxFireShot@users.noreply.github.com>
2024-01-15 13:21:05 -0300

    add roomModel and enums

---
+++ api/src/models/enums.ts
@@ -0,0 +1,25 @@
+export enum RoomStatus {
+    OPEN,
+    CLOSED
+}
+
+export enum Mode {
+    SINGLE_PLAYER,
+    MULTIPLAYER
+}
+
+export enum PrivacyLevel {
+    PUBLIC,
+    PRIVATE
+}
+
+export enum MatchStatus {
+    READY,
+    STARTED,
+    FINISHED
+}
+
+export enum Color {
+    BLACK,
+    WHITE
+}
--- api/src/models/roomModel.ts
+++ api/src/models/roomModel.ts
@@ -1,3 +1,17 @@
+import { RoomStatus, Mode, PrivacyLevel } from "./enums"
+
 export interface RoomModel {
-    
+    id: String,
+    name: String,
+    status: RoomStatus,
+    mode: Mode,
+    chatMode: PrivacyLevel,
+    visibility: PrivacyLevel,
+    roomUrl: String,
+    playerOne: String,
+    playerTwo: String,
+    playerOneScore: Number, 
+    playerTwoScore: Number, 
+    viewers: Number, 
+    createdAt: Date,
 }
