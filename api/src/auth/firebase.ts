067e7bfa0b910fc3d4b06fba1cb9df2c951de281
Vinicius Rocha <the.vinicius.rocha@gmail.com>
2024-01-19 15:07:37 -0300

    removed unused firebase config.

--- api/src/auth/firebase.ts
+++ api/src/auth/firebase.ts
@@ -1,18 +1,7 @@
 import { initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
-import { env } from "process";
 
 var admin = require("firebase-admin");
-
-const firebaseConfig = {
-    apiKey: env.apiKey,
-    authDomain: env.authDomain,
-    projectId: env.projectId,
-    storageBucket: env.storageBucket,
-    messagingSenderId: env.messagingSenderId,
-    appId: env.appId,
-    measurementId: env.measurementId,
-};
 
 const firebase = initializeApp({credential: admin.credential.cert("./firebase-service-account.json")});
 const auth = getAuth(firebase);
